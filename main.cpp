ad68247d7d0376cca28933615162e0848071ee7b
Kolos Tamara <tamarakolos.375@gmail.com>
2021-10-11 11:54:26 +0300

    newFeature commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    int a, b;
+    freopen("aplusb.in", "r", stdin);
+    freopen("aplusb.out", "w", stdout);
+    std::cin >> a >> b;
+    std::cout << a + b << '\n';
+    return 0;
+}
